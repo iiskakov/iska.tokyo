0b96ad83bcebf89d21c4ac642c6a78410abe05f1
iiskakov <iiskakov@student.42.us.org>
2021-01-21 11:10:48 +0600

    Favicon + some stuff

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,20 +7,9 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import "./layout.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
 
   return (
     <>
@@ -35,9 +24,7 @@ const Layout = ({ children }) => {
             marginTop: `2rem`,
           }}
         >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
+          © {new Date().getFullYear()}
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -25,59 +25,59 @@ const IndexPage = () => {
     <Layout>
       <div
         onMouseMove={() => updateStyles({ DSPL: randomSetting() })}
-        style={{ maxWidth: `40%`, minHeight: `600px` }}
+        onTap={() => updateStyles({ DSPL: randomSetting() })}
+        style={{ width: `100%`, minHeight: `600px` }}
       >
-        <h1
-          style={{
-            ...customStyles,
-            color: `#760000`,
-            fontSize: `15vh`,
-            fontWeight: `400`,
-          }}
-        >
-          iska
-        </h1>
-        <p>
-          Sup, I'm Iskander.
+        <div style={{ paddingLeft: `0.5rem` }}>
+          <h1
+            style={{
+              ...customStyles,
+              color: `#760000`,
+              fontSize: `12vh`,
+              fontWeight: `400`,
+            }}
+          >
+            iska
+          </h1>
+          <p>
+            Sup, I'm Iskander.
+            <br />
+            I'm a student at 42 Silicon Valley.
+            <br />I once founded an{" "}
+            <Link to="https://kapital.kz/business/54006/kak-prodavat-morozhenoye-s-ledyanoy-skovorody.html">
+              ice-cream
+            </Link>{" "}
+            <Link to="https://www.buro247.kz/lifestyle/city/novoe-mesto-na-karte-almaty-kafe-morozhenoe-kosm-z.html">
+              brand
+            </Link>{" "}
+            <br />
+          </p>
+          <p>
+            Curious about: functional programming, generative design and complex
+            systems.
+            <br />
+            Trying to master: stoicism, brazilian jiu-jistu, meditation.
+          </p>
+          <p>
+            First computer: 1999
+            <br />
+            First programming language: C<br />
+            Text editor: Doom Emacs.
+            <br />
+            Currently learning: Clojure
+            <br />
+            Recent project:{" "}
+            <Link to="https://optimistic-kowalevski-550adb.netlify.app/">
+              IKEA Social Enterpreneurship
+            </Link>{" "}
+          </p>
+          <Link to="https://www.instagram.com/iska/">instagram.com/iska</Link>{" "}
           <br />
-          I'm a student at 42 Silicon Valley.
-          <br />I once founded an{" "}
-          <Link to="https://kapital.kz/business/54006/kak-prodavat-morozhenoye-s-ledyanoy-skovorody.html">
-            ice-cream
-          </Link>{" "}
-          <Link to="https://www.buro247.kz/lifestyle/city/novoe-mesto-na-karte-almaty-kafe-morozhenoe-kosm-z.html">
-            brand
-          </Link>{" "}
+          <Link to="https://www.are.na/iska">are.na/iska</Link> <br />
+          <Link to="https://www.goodreads.com/3005 ">goodreads.com/3005</Link>
           <br />
-        </p>
-        <p>
-          Now interested in functional programming, generative design and
-          complex systems.
-          <br />
-          Most recent project:{" "}
-          <Link to="https://optimistic-kowalevski-550adb.netlify.app/">
-            IKEA Social Enterpreneurship
-          </Link>{" "}
-        </p>
-        <p>
-          First computer: 1999
-          <br />
-          First programming language: C<br />
-          Text editor: Doom Emacs.
-          <br />
-          Currently obsessed with: Clojure
-          <br />
-          Recent project:{" "}
-        </p>
-        <Link to="https://www.instagram.com/iska/">
-          instagram.com/iska
-        </Link>{" "}
-        <br />
-        <Link to="https://www.are.na/iska">are.na/iska</Link> <br />
-        <Link to="https://www.goodreads.com/3005 ">
-          goodreads.com/3005
-        </Link>{" "}
-        <br />
+          <Link to="https://t.me/iskander3005">t.me/iskander3005</Link>
+        </div>
       </div>
     </Layout>
   )
