2892140eb5e6a3488ae4dcddce889be91a63cb93
iiskakov <iiskakov@student.42.us.org>
2021-01-21 20:27:00 +0600

    Are you winning son?

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,74 @@
 import React from "react"
 import { Link } from "gatsby"
 import useVariableFont from "react-variable-fonts"
-
+import ReactCursorPosition, {
+  INTERACTIONS,
+} from "@appinfini/react-cursor-position"
+import Pointable from "react-pointable"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import useMousePosition from "../components/useMousePosition"
 
-const initialSettings = {
-  DSPL: 100,
+const PositionLabel = props => {
+  const {
+    detectedEnvironment: {
+      isMouseDetected = false,
+      isTouchDetected = false,
+    } = {},
+    elementDimensions: { width = 0, height = 0 } = {},
+    isActive = false,
+    isPositionOutside = false,
+    position: { x = 0, y = 0 } = {},
+  } = props
+
+  return (
+    <div className="example__label">
+      <h1
+        style={{
+          color: `#760000`,
+          fontSize: `15vh`,
+          fontWeight: `400`,
+          fontVariationSettings: `"DSPL" ${y}`,
+        }}
+      >
+        iska{" "}
+      </h1>
+    </div>
+  )
 }
 
 const IndexPage = () => {
-  const { x, y } = useMousePosition()
-  const [normalStyles] = useVariableFont("TTAlientzVar", "normal")
-  const [customStyles, updateStyles] = useVariableFont(
-    "TTAlientzVar",
-    initialSettings
-  )
-
-  const randomSetting = () => y
-
   return (
     <Layout>
-      <div
-        onMouseMove={() => updateStyles({ DSPL: randomSetting() })}
-        onTouchMove={() => updateStyles({ DSPL: randomSetting() })}
+      <ReactCursorPosition
         style={{ width: `100%`, minHeight: `600px` }}
+        activationInteractionTouch={INTERACTIONS.TOUCH}
       >
+        <PositionLabel />
         <div style={{ paddingLeft: `0.5rem` }}>
-          <h1
-            style={{
-              ...customStyles,
-              color: `#760000`,
-              fontSize: `15vh`,
-              fontWeight: `400`,
-            }}
-          >
-            iska
-          </h1>
           <p>
             † Sup, I'm Iskander.
             <br />
             † I'm a student at 42 Silicon Valley.
             <br />† I once founded an{" "}
-            <Link target="_blank" to="https://kapital.kz/business/54006/kak-prodavat-morozhenoye-s-ledyanoy-skovorody.html">
+            <Link
+              target="_blank"
+              to="https://kapital.kz/business/54006/kak-prodavat-morozhenoye-s-ledyanoy-skovorody.html"
+            >
               ice-cream
             </Link>{" "}
-            <Link target="_blank" to="https://www.buro247.kz/lifestyle/city/novoe-mesto-na-karte-almaty-kafe-morozhenoe-kosm-z.html">
+            <Link
+              target="_blank"
+              to="https://www.buro247.kz/lifestyle/city/novoe-mesto-na-karte-almaty-kafe-morozhenoe-kosm-z.html"
+            >
               brand.
             </Link>{" "}
             <br />
           </p>
           <p>
             † Curious about: functional programming and generative design.
-            <br />
-            † Trying to master: stoicism, brazilian jiu-jistu, meditation.
+            <br />† Trying to master: stoicism, brazilian jiu-jistu, meditation.
           </p>
           <p>
             † First computer: 1999
@@ -64,20 +77,31 @@ const IndexPage = () => {
             † Text editor: Doom Emacs.
             <br />
             † Currently learning: Clojure
-            <br />
-            † Recent project:{" "}
-            <Link target="_blank" to="https://optimistic-kowalevski-550adb.netlify.app/">
+            <br />† Recent project:{" "}
+            <Link
+              target="_blank"
+              to="https://optimistic-kowalevski-550adb.netlify.app/"
+            >
               IKEA Social Enterpreneurship
             </Link>{" "}
           </p>
-          <Link target="_blank" to="https://www.instagram.com/iska/">instagram.com/iska</Link>{" "}
+          <Link target="_blank" to="https://www.instagram.com/iska/">
+            instagram.com/iska
+          </Link>{" "}
           <br />
-          <Link target="_blank" to="https://www.are.na/iska">are.na/iska</Link> <br />
-          <Link target="_blank" to="https://www.goodreads.com/3005 ">goodreads.com/3005</Link>
+          <Link target="_blank" to="https://www.are.na/iska">
+            are.na/iska
+          </Link>{" "}
           <br />
-          <Link target="_blank" to="https://t.me/iskander3005">t.me/iskander3005</Link>
+          <Link target="_blank" to="https://www.goodreads.com/3005 ">
+            goodreads.com/3005
+          </Link>
+          <br />
+          <Link target="_blank" to="https://t.me/iskander3005">
+            t.me/iskander3005
+          </Link>
         </div>
-      </div>
+      </ReactCursorPosition>
     </Layout>
   )
 }
